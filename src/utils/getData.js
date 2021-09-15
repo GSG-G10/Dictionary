c87792574a04e0fdc100e436df1490dcd3225411
reemsh2000 <sh951652@gmail.com>
2021-09-15 16:37:20 +0300

    check response function

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,6 +1,7 @@
+import checkResponse from "./checkResponse";
 const getData = (query) => {
   return fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${query}`)
-    .then((res) => res.json())
+  .then(checkResponse)
     .catch((err) => {
       throw new Error(`fetch getData failed ${err}`);
     });
