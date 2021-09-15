09ff7b8261c6b629ff86c0429ba6c12a8d8c7d95
divluffy <div.jo2022@gmail.com>
2021-09-15 11:45:44 +0300

    code header

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Main extends Component {
+    render() {
+        return (
+            <main>
+                <div className="bowl_main">
+                main is
+                </div>
+          </main>
+        )
+    }
+}
+
+export default Main
