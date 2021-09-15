9cf4006c31f60736d8055aee4265d50fe2c20fb0
reemsh2000 <sh951652@gmail.com>
2021-09-15 15:33:29 +0300

    get data from header and display in cards

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,19 @@
 import React from "react";
-
- function Main (props) {
-
-console.log(props)
-
-    return (
-      <main>
-        <div className="bowl_main">{props.cardData}</div>
-      </main>
-    );
+import Card from "./Card";
+function Main(props) {
+  const { cardsData } = props;
+  return (
+    <main>
+      <div className="bowl_main">
+        <ul>
+        {cardsData ? cardsData.map((ele,i) => <Card cardKey={i}word={ele.word} meanings={ele.meanings} />)
+          : ""}
+          </ul>
+      </div>
+    </main>
+  );
 }
 
 export default Main;
+
+   // return console.log( `key=${i}, word=${ele.word} ,meanings=${ele.meanings}`)
